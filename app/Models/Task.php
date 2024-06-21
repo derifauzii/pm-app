c3a14e1a7145571012e8e6212d939d2ae2549141
derdorrr <derifauzi27@gmail.com>
2024-06-21 17:11:53 +0700

    commit1

---
+++ app/Models/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class,);
+    }
+
+    public function assignedUser()
+    {
+        return $this->belongsTo(User::class, 'assigned_user_id');
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(Project::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(Project::class, 'updated_by');
+    }
+}
